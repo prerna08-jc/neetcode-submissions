e874247820edb90efad2f6798569478be6cc139e
Prerna Choudhary <prerna.choudhary2023@vitstudent.ac.in>
2026-07-27 00:45:17 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    int trap(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int lmax = height[l];
+        int rmax = height[r];
+        int res = 0;
+        while (l < r) {
+            if (lmax < rmax) {
+                l++;
+                lmax = max(lmax, height[l]);
+                res += lmax - height[l];
+            } else {
+                r--;
+                rmax = max(rmax, height[r]);
+                res += rmax - height[r];
+            }
+        }
+        return res;
+    }
+};
