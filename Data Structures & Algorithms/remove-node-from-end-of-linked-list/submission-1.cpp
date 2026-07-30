9d105e2653c0997e788887e64bc6f3856760d226
Prerna Choudhary <prerna.choudhary2023@vitstudent.ac.in>
2026-07-30 15:34:56 +0530

    Refactor removeNthFromEnd function
    Refactor removeNthFromEnd function to simplify logic and remove unnecessary dummy node.

--- Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.cpp
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.cpp
@@ -8,27 +8,23 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
-   public:
+public:
+
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* dummy = new ListNode(0, head);
-        ListNode* fast = dummy;
-        ListNode* slow = dummy;
-        // Move `fast` pointer n + 1 steps ahead
-        for (int i = 0; i <= n; i++) {
+        ListNode* fast = head;
+        for(int i = 0 ; i < n; i++){
             fast = fast->next;
         }
-        // Move both pointers until `fast` reaches the end
-        while (fast != nullptr) {
+        if (!fast) return head->next;
+        ListNode* slow = head;
+        while(fast->next){
             fast = fast->next;
             slow = slow->next;
         }
-        // `slow->next` is the target node to delete
-        ListNode* nodeToDelete = slow->next;
         slow->next = slow->next->next;
-        delete nodeToDelete;
-        ListNode* newHead = dummy->next;
-        delete dummy;  
-        return newHead;
+        return head;
+        
     }
 };
