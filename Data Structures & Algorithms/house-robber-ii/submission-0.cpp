9e68f60f67e5cbe9b61e91d521c684c834361269
Prerna Choudhary <prerna.choudhary2023@vitstudent.ac.in>
2026-09-10 18:47:44 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    int robLinear(vector<int>& nums, int start, int end) {
+        int prev2 = 0;
+        int prev1 = 0;
+        for (int i = start; i <= end; i++) {
+            int curr = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        int case1 = robLinear(nums, 0, n - 2);  //Rob from house 0 to n - 2
+        int case2 = robLinear(nums, 1, n - 1);  //Rob from house 1 to n - 1
+        return max(case1, case2);
+    }
+};
