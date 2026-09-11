9f9af970dc7016e52e48a67e26514896b1be49c5
Prerna Choudhary <prerna.choudhary2023@vitstudent.ac.in>
2026-09-12 00:27:03 +0530

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end());
+        res.push_back(intervals[0]);
+        int n = intervals.size();
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] > res.back()[1]) {
+                res.push_back(intervals[i]);
+            } else {
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            }
+        }
+        return res;
+    }
+};
